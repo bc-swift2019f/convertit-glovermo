e43c9e9c7611ad68e1a259068e794a5a6b983a8c
Morgan Glover <glovermo@bc.edu>
2019-09-30 07:41:26 -0400

    6.5 Completed - Guard statements, segmented index

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var fromUnitsLabel: UILabel!
     @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
+    @IBOutlet weak var decimalSegment: UISegmentedControl!
+    
     
     var formulaArray = ["miles to kilometers",
                         "kilometers to miles",
@@ -29,38 +31,45 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         formulaPicker.dataSource = self
         formulaPicker.delegate = self
+        conversionString = formulaArray[formulaPicker.selectedRow(inComponent: 0)]
     }
     
     func calculateConversion() {
-        
-        var outputValue = 0.0
-        if let inputValue = Double(userInput.text!) {
-            
-            switch conversionString {
-            case "miles to kilometers":
-                outputValue = inputValue / 0.62137
-            case "kilometers to miles":
-                outputValue = inputValue * 0.62137
-            case "feet to meters":
-                outputValue = inputValue / 3.2808
-            case "yards to meters":
-                outputValue = inputValue / 1.0936
-            case "meters to feet":
-                outputValue = inputValue * 3.2808
-            case "meters to yards":
-                outputValue = inputValue * 1.0936
-            default:
-                print("show alert - for some reason we didn't have a conversion string")
-            }
-            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
-        } else {
+        guard let inputValue = Double(userInput.text!) else {
             print("show alert here to say the value entered was not a number")
+            return
         }
         
+        var outputValue = 0.0
+        switch conversionString {
+        case "miles to kilometers":
+            outputValue = inputValue / 0.62137
+        case "kilometers to miles":
+            outputValue = inputValue * 0.62137
+        case "feet to meters":
+            outputValue = inputValue / 3.2808
+        case "yards to meters":
+            outputValue = inputValue / 1.0936
+        case "meters to feet":
+            outputValue = inputValue * 3.2808
+        case "meters to yards":
+            outputValue = inputValue * 1.0936
+        default:
+            print("show alert - for some reason we didn't have a conversion string")
+        }
 
+        let formatString = (decimalSegment.selectedSegmentIndex < decimalSegment.numberOfSegments-1 ? "%.\(decimalSegment.selectedSegmentIndex+1)f" : "%f")
+        let outputString = String(format: formatString, outputValue)
+        resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputString) \(toUnits)"
     }
+    
+    @IBAction func decimalSelected(_ sender: UISegmentedControl) {
+        calculateConversion()
+    }
+    
 
     @IBAction func convertButtonPressed(_ sender: UIButton) {
+        calculateConversion()
     }
     
 }
